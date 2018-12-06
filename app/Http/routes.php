8cd69f1e6f95dd1d708f0b6c3b483162dff43c82
yuse <lyzwhh@qq.com>
2018-12-06 19:26:20 +0800

    add migration

---
+++ app/Http/routes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuse
+ * Date: 2018/12/6
+ * Time: 19:20
+ */
---
+++ database/migrations/2018_12_06_084456_create_reports_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('tag1')->default('👋🐟');
+            $table->string('tag2');
+            $table->string('tag3');
+
+            //TODO: 时间的表示方式
+            $table->integer('create_at');  // Carbon 日级规范
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
